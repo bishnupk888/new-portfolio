91a26b9df6b460082b7d286958662ee52bd07bad
Bishnu PK <bishnupk888@gmail.com>
2026-03-18 13:59:49 +0530

    Update resume link to Google Drive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -158,7 +158,7 @@ function Hero() {
           View Projects
         </a>
         <a
-          href="/resume.pdf"
+          href="https://drive.google.com/file/d/1kixWQxo4mw2Si4cpT08TiVAugh9atpoZ/view?usp=sharing"
           target="_blank"
           className="glass-btn px-8 py-4 text-lg font-bold"
         >
